cc1225a943371db5d2a4dd9d0377610bf5187f4e
the-Zacrux <divyanshshukla705@gmail.com>
2026-06-16 23:29:50 +0530

    Add files via upload

---
+++ program67.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class program67 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of first array: ");
+        int n1 = sc.nextInt();
+        int arr1[] = new int[n1];
+
+        System.out.println("Enter elements of first array:");
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter size of second array: ");
+        int n2 = sc.nextInt();
+        int arr2[] = new int[n2];
+
+        System.out.println("Enter elements of second array:");
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        System.out.println("Intersection of arrays:");
+
+        for (int i = 0; i < n1; i++) {
+            boolean found = false;
+
+            for (int j = 0; j < n2; j++) {
+                if (arr1[i] == arr2[j]) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                System.out.print(arr1[i] + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
